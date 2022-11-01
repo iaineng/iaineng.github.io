db1f1f8b654d016aa41865835ad0cf92f9d11bea
JiaxinZeng <zengjiaxin23@outlook.com>
2022-11-01 10:56:03 +0800

    🫥

--- scripts/universal/huo-yan-bei-fucker.meta.js
+++ scripts/universal/huo-yan-bei-fucker.meta.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Huo Yan Bei Fucker
 // @match        *://jinshuju.net/*
-// @version      0.1.3
+// @version      0.1.4
 // @author       lth,zjx
 // @run-at       document-start
 // @grant        GM_log
+// @grant        unsafeWindow
 // @updateURL    https://zengjx.tk/scripts/universal/huo-yan-bei-fucker.meta.js
 // @downloadURL  https://zengjx.tk/scripts/universal/huo-yan-bei-fucker.user.js
 // ==/UserScript==
