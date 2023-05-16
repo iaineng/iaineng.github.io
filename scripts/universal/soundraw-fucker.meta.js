314bac422e5d086cf3277fadd9300dea6f295128
iaineng <zengjiaxin23@outlook.com>
2023-05-16 17:29:29 +0800

    Create soundraw-fucker.meta.js

---
+++ scripts/universal/soundraw-fucker.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Soundraw Fucker
+// @match        https://soundraw.io/*
+// @version      0.1.0
+// @author       zjx
+// @grant        unsafeWindow
+// @run-at       document-start
+// @updateURL    https://zengjx.tk/scripts/universal/soundraw-fucker.meta.js
+// @downloadURL  https://zengjx.tk/scripts/universal/soundraw-fucker.user.js
+// ==/UserScript==
